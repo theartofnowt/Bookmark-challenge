262bf99aa22f9b52d983bfffb9c56b2f1173c8ba
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-27 11:06:45 +0100

    Michael heroku done

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,7 +12,7 @@ property :url,   String
 
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_development")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 #DataMapper.setup(:default, "postgres://localhost/bookmark_manager_development")
 DataMapper.finalize
 DataMapper.auto_upgrade!
