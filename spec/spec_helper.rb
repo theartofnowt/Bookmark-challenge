6e488da19a74a4d2758c070730568fd5e8b621c2
nandhini <nandhini31@gmail.com>
2017-07-27 10:04:52 +0100

    Fixed heroku

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 ENV['RACK_ENV'] = "test"
-require File.join(File.dirname(__FILE__), '../app', 'app.rb')
+#require File.join(File.dirname(__FILE__), '../app', 'app.rb')
 require 'capybara/rspec'
 require 'capybara'
 require 'rspec'
 require 'database_cleaner'
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative '../app/app.rb'
 
 Capybara.app = BookmarkManager
 
@@ -23,14 +26,14 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
+  #
+  # config.expect_with :rspec do |expectations|
+  #   expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  # end
+  # config.mock_with :rspec do |mocks|
+  #   mocks.verify_partial_doubles = true
+  # end
+  # config.shared_context_metadata_behavior = :apply_to_host_groups
+  #
 
 end
